22f7f93369e3eaf703f0253162c61746ed81c0cb
jinwoole <indibooks@naver.com>
2022-12-15 15:59:20 +0900

    complete test of integral_constant

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <type_traits>
 #include "./type_traits.hpp"
 
 //enable_if test
 template<typename T, typename ft::enable_if<ft::is_integral<T>::value, int>::type = 0>
 void foo(void) { std::cout << "true\n"; }
+
+//integral_constant test
+template <unsigned n>
+struct factorial : ft::integral_constant<int,n * factorial<n-1>::value> {};
+
+template <>
+struct factorial<0> : ft::integral_constant<int,1> {};
 
 
 int main() {
@@ -21,5 +29,9 @@ int main() {
     std::cout << "char: " << ft::is_integral<char>::value << std::endl;
     std::cout << "int: " << ft::is_integral<int>::value << std::endl;
     std::cout << "float: " << ft::is_integral<float>::value << std::endl;
+
+    //integral_constant test
+    std::cout << "[Integral_constant test]" << std::endl;
+    std::cout << factorial<5>::value;
     return 0;
 }
